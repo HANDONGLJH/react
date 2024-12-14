85861ca64510f104d628ad0166126b3797bb6328
HANDONGLJH <93640401+HANDONGLJH@users.noreply.github.com>
2024-12-14 20:10:31 +0900

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,12 +8,36 @@ const Signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // 비밀번호 확인
     if (password !== confirmPassword) {
       alert("비밀번호가 일치하지 않습니다.");
       return;
     }
-    // 회원가입 로직 처리
+
+    // 새 사용자 데이터 생성
+    const newUser = {
+      email,
+      password,
+    };
+
+    // 기존 사용자 목록 가져오기
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+    // 중복 이메일 확인
+    const isExistingUser = existingUsers.some((user) => user.email === email);
+    if (isExistingUser) {
+      alert("이미 존재하는 이메일입니다.");
+      return;
+    }
+
+    // 새로운 사용자 추가
+    existingUsers.push(newUser);
+    localStorage.setItem("users", JSON.stringify(existingUsers));
     alert("회원가입 성공!");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
   };
 
   return (
